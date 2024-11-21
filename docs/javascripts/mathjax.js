217311b4dc2f50a8dba5f4de97c39d4dbbbd1b27
Alice Lim <lexiestrelka@gmail.com>
2024-11-21 17:12:15 +0800

    Add mathjax.

---
+++ docs/javascripts/mathjax.js
@@ -0,0 +1,22 @@
+// This file is used by mkdocs.yml to install MathJax into the website, allowing LaTeX in Markdown
+// documents to be rendered.
+
+window.MathJax = {
+    tex: {
+      inlineMath: [["\\(", "\\)"]],
+      displayMath: [["\\[", "\\]"]],
+      processEscapes: true,
+      processEnvironments: true
+    },
+    options: {
+      ignoreHtmlClass: ".*|",
+      processHtmlClass: "arithmatex"
+    }
+  };
+  
+document$.subscribe(() => { 
+    MathJax.startup.output.clearCache()
+    MathJax.typesetClear()
+    MathJax.texReset()
+    MathJax.typesetPromise()
+})
